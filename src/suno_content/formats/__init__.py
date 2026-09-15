6123df440c7da64dc6ee4395cd0569ec230d5b13
Pablo Marchina <pablomarchina@gmail.com>
2026-09-15 10:35:27 -0300

    W002-T004: export format contracts

---
+++ src/suno_content/formats/__init__.py
@@ -0,0 +1,33 @@
+from .contracts import (
+    ArticleOutput,
+    ArticleParagraph,
+    ArticleSection,
+    CarouselOutput,
+    CarouselRole,
+    CarouselSlide,
+    FormatContractError,
+    FormatKind,
+    FormatOutput,
+    ShortVideoOutput,
+    ShortVideoSegment,
+    SourceRef,
+    VideoRole,
+    parse_format_output,
+)
+
+__all__ = [
+    "ArticleOutput",
+    "ArticleParagraph",
+    "ArticleSection",
+    "CarouselOutput",
+    "CarouselRole",
+    "CarouselSlide",
+    "FormatContractError",
+    "FormatKind",
+    "FormatOutput",
+    "ShortVideoOutput",
+    "ShortVideoSegment",
+    "SourceRef",
+    "VideoRole",
+    "parse_format_output",
+]
