4aaaac284dc957846d528c80cd394c68184a665f
Pablo Marchina <pablomarchina@gmail.com>
2026-09-22 21:31:17 -0300

    feat: expose live cockpit projection API

--- src/suno_content/cockpit/__init__.py
+++ src/suno_content/cockpit/__init__.py
@@ -6,6 +6,16 @@ from .adapters import (
     load_run_state_from_history,
     load_run_state_from_sqlite,
 )
+from .live import (
+    AuthorizationProjectionError,
+    AuthorizedScope,
+    CANDIDATES,
+    ContractProjectionError,
+    LiveCockpitProjector,
+    LiveCockpitState,
+    reconnect,
+    render_candidate,
+)
 from .models import (
     CockpitSnapshot,
     EvidenceItem,
@@ -18,10 +28,16 @@ from .models import (
 from .render import render_html
 
 __all__ = [
+    "AuthorizationProjectionError",
+    "AuthorizedScope",
+    "CANDIDATES",
     "CockpitSnapshot",
+    "ContractProjectionError",
     "EvidenceItem",
     "EvidenceState",
     "JobEvidence",
+    "LiveCockpitProjector",
+    "LiveCockpitState",
     "Provenance",
     "RepairEvidence",
     "TelemetryEvidence",
@@ -29,5 +45,7 @@ __all__ = [
     "load_json_artifact",
     "load_run_state_from_history",
     "load_run_state_from_sqlite",
+    "reconnect",
+    "render_candidate",
     "render_html",
 ]
